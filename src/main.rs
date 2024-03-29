5d12a149cad946a260dbcc4823f932a9ea0184e4
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2024-03-29 01:41:40 +0100

    chore: remove unnecessary `gix` boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::{fs, path::Path};
 
 use chrono::NaiveDate;
 use color_eyre::eyre::{eyre, Result, WrapErr};
-use gix::Repository;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_until},
@@ -15,7 +14,7 @@ fn main() -> Result<()> {
     let dest = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/this-week-in-rust"));
 
     println!("Setting up repository...");
-    let _repo = git_clone_or_open("https://github.com/rust-lang/this-week-in-rust.git", dest)?;
+    git_clone_or_open("https://github.com/rust-lang/this-week-in-rust.git", dest)?;
 
     let mut collection = Vec::new();
 
@@ -139,7 +138,7 @@ fn take_quote(input: &str) -> Result<&str> {
 
 /// Try to open repository at `dest`, if directory exists,
 /// otherwise clone from remote url `src` into local directory `dest`.
-fn git_clone_or_open(src: &str, dest: &Path) -> Result<Repository> {
+fn git_clone_or_open(src: &str, dest: &Path) -> Result<()> {
     // SAFETY: The closure doesn't use mutexes or memory allocation, so it should be safe to call from a signal handler.
     unsafe {
         gix::interrupt::init_handler(1, || {})?;
@@ -148,6 +147,7 @@ fn git_clone_or_open(src: &str, dest: &Path) -> Result<Repository> {
     if dest.is_dir() {
         println!("Repository at '{}' is present. Opening...", dest.display());
         gix::open(dest)
+            .map(|_| ())
             .wrap_err_with(|| format!("failed to open repository at '{}'", dest.display()))
     } else {
         git_clone(src, dest)
@@ -156,10 +156,10 @@ fn git_clone_or_open(src: &str, dest: &Path) -> Result<Repository> {
 }
 
 /// Clone git repository from remote url `src` into local directory `dest`.
-fn git_clone(src: &str, dest: &Path) -> Result<Repository> {
+fn git_clone(src: &str, dest: &Path) -> Result<()> {
     let url = gix::url::parse(src.into())?;
 
-    println!("Cloning {url:?} into '{}'...", dest.display());
+    println!("Cloning '{src}' into '{}'...", dest.display());
     let (mut prepare_checkout, _) = gix::prepare_clone(url, dest)?
         .fetch_then_checkout(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
 
@@ -168,24 +168,7 @@ fn git_clone(src: &str, dest: &Path) -> Result<Repository> {
         prepare_checkout.repo().work_dir().expect("should be there")
     );
 
-    let (repo, _) =
-        prepare_checkout.main_worktree(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
+    prepare_checkout.main_worktree(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
 
-    let remote = repo
-        .find_default_remote(gix::remote::Direction::Fetch)
-        .expect("always present after clone")?;
-
-    println!(
-        "Default remote: {} -> {}",
-        remote
-            .name()
-            .expect("default remote is always named")
-            .as_bstr(),
-        remote
-            .url(gix::remote::Direction::Fetch)
-            .expect("should be the remote URL")
-            .to_bstring(),
-    );
-
-    Ok(repo)
+    Ok(())
 }
